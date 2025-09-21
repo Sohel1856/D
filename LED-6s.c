6fb68341ff13124e2b0c7fa0507e09d7084f9474
Sohel1856 <168741665+Sohel1856@users.noreply.github.com>
2025-09-22 01:02:16 +0600

    Update LED-6s.c

--- LED-6s.c
+++ LED-6s.c
@@ -1,29 +1,15 @@
-// Custom delay function (variable ms)
-void myDelay_ms(unsigned int time_ms) {
-    unsigned int i;
-    for(i = 0; i < time_ms; i++) {
-        Delay_ms(1);   // Built-in mikroC constant delay
-    }
-}
-
 void main() {
-    unsigned int on_time, off_time;
-
-    TRISB = 0x00;   // PORTB as output
-    PORTB = 0x00;   // All pins OFF initially
-
+    TRISB = 0x00;
+    PORTB = 0x00;
     while(1) {
-        // Cycle: ON time increases 1 ? 5 sec, OFF time decreases 5 ? 1 sec
-        for(on_time = 1; on_time <= 5; on_time++) {
-            off_time = 6 - on_time;   // Keep total cycle time = 6 sec
-
+    int i;
+        for(i = 1000; i < 6000; i+=1000) {
             // LED ON
-            PORTB.F0 = 1;             // Turn ON LED at RB0
-            myDelay_ms(on_time * 1000); // Delay ON_time seconds
-
+            PORTB.F0 = 1;  
+            vdelay_ms(i);           // Turn ON LED at RB0
             // LED OFF
             PORTB.F0 = 0;             // Turn OFF LED
-            myDelay_ms(off_time * 1000); // Delay OFF_time seconds
+            vdelay_ms(6000- i);
         }
     }
 }
